2a1e6074ec01817910b77dc549be003a06b86f24
XvKuoMing <kamasalyamov@gmail.com>
2025-07-02 10:05:49 +0300

    upd

--- src/rtp_llm/utils/__init__.py
+++ src/rtp_llm/utils/__init__.py
@@ -1,9 +1,9 @@
 from .audio_processing import (
-    resample_pcm16, 
+    StreamingResample, 
     pcm2wav, 
     ulaw2pcm, 
     alaw2pcm, 
     opus2pcm
 )
 
-__all__ = ["resample_pcm16", "pcm2wav", "ulaw2pcm", "alaw2pcm", "opus2pcm"]
+__all__ = ["StreamingResample", "pcm2wav", "ulaw2pcm", "alaw2pcm", "opus2pcm"]
