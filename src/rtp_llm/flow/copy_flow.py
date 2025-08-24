3a243f40825f74720beb3070dc80a1e0ff1d912b
XvKuoMing <kamasalyamov@gmail.com>
2025-08-24 14:57:26 +0300

    add window flow

--- src/rtp_llm/flow/copy_flow.py
+++ src/rtp_llm/flow/copy_flow.py
@@ -8,17 +8,22 @@ logger = logging.getLogger(__name__)
 class CopyFlowManager(BaseChatFlowManager):
 
 
-    def __init__(self):
+    def __init__(self, throttle_windows: int = 3):
         self.last_state = VoiceState.SILENCE
+        self.throttle_counter = 0
+        self.throttle_windows = throttle_windows
     
 
     async def run_agent(self, voice_state: VoiceState) -> bool:
         """
         recieves vad voice state and returns if the agent should be run
         """
+        if (self.last_state == VoiceState.SPEECH and voice_state == VoiceState.SILENCE):
+            self.throttle_counter += 1
 
-        if self.last_state == VoiceState.SPEECH and voice_state == VoiceState.SILENCE:
+        if self.throttle_counter >= self.throttle_windows:
             # if user stopped speaking, we should run the agent
+            self.throttle_counter = 0
             return True
         self.last_state = voice_state
         return False
